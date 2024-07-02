cc49b417ffa044460bea2be1cb3dfa93b13b49d1
Samnan Ullah Jan <62881005+SamnanUllahJan@users.noreply.github.com>
2024-07-03 01:50:06 +0500

    Initial commit

---
+++ src/SockifychatServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Samnanullahjan\Sockifychat;
+
+use Illuminate\Support\ServiceProvider;
+
+class SockifychatServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // Publishing configurations, migrations, views, etc.
+    }
+
+    public function register()
+    {
+        // Bind any services into the container.
+    }
+}
